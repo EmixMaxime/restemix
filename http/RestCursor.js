7282b174595614da5428bd0c89a0dd8201725b69
EmixMaxime <emixmaxime@outlook.fr>
2017-02-14 13:45:44 +0100

    Add sort method to the RestCursor.js
    Add possibility to sort my items from database (great for index action)

--- http/RestCursor.js
+++ http/RestCursor.js
@@ -1,4 +1,6 @@
-const RestCursor = function () {
+const _includes = require('lodash/includes');
+
+const RestCursor = function ({ _includes }) {
 
   return (req, cursor) => ({
     limit () {
@@ -7,13 +9,44 @@ const RestCursor = function () {
 
       return restCursor(req, Object.assign({}, cursor, limit));
     },
+    // GET  https://emixmaxime.com/books?sort=title,author&desc=title&asc=author
+    sort () {
+      const sortCursor = {};
+      let { sort, desc, asc } = req.query;
+      if (!sort) return restCursor(req, cursor);
+
+      desc = desc.split(',');
+      asc = asc.split(',');
+
+      // Un champ ne peut pas être dans desc et asc -> erreur de requête
+      // Un champ doit être dans sort si il est dans asc|desc
+      desc.forEach(d => {
+        if (!_includes(sort, d)) throw new Error();
+        asc.forEach(a => {
+          if (!_includes(sort, a)) throw new Error();
+          if (d === a) throw new Error();
+        });
+      });
+
+      // Gestion du desc
+      desc.forEach(d => {
+        sortCursor[d] = -1;
+      });
+
+      // Gestion du asc
+      asc.forEach(a => {
+        sortCursor[a] = 1;
+      });
+
+      return restCursor(req, Object.assign({}, cursor, sortCursor));
+    },
 
     get: () => cursor
   });
 
 };
 
-const restCursor = RestCursor();
+const restCursor = RestCursor({ _includes });
 
 const GetRestCursor = function ({ restCursor }) {
   return (req) => {
