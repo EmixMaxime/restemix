f839f242d85d1c67fd5f606f0831143d0bfa449f
EmixMaxime <emixmaxime@outlook.fr>
2017-02-14 21:46:05 +0100

    Add CanCanCan dependencies

--- http/Controllers/ControllerServiceProvider.js
+++ http/Controllers/ControllerServiceProvider.js
@@ -5,8 +5,10 @@ const Controller = require('./Controller');
 const getResourceName = require('./utils/getResourceName');
 const updateDatabase = require('./utils/updateDatabase');
 
+const CanCan = require('cancancan');
+
 const controllerServiceProvider = (dependencies) => Controller.bind(null, dependencies);
-const controller = controllerServiceProvider({getRestFilters, getRestCursor, getResourceName, updateDatabase, fillSchema});
+const controller = controllerServiceProvider({getRestFilters, getRestCursor, getResourceName, updateDatabase, fillSchema, CanCan});
 
 module.exports = {
   controllerServiceProvider,
