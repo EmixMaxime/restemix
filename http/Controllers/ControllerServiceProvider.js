ace1407c1837ff0024e0590d4e3eb09679589826
EmixMaxime <emixmaxime@outlook.fr>
2017-02-23 16:57:19 +0100

    Delete the updatedatabase.js (useless file)

--- http/Controllers/ControllerServiceProvider.js
+++ http/Controllers/ControllerServiceProvider.js
@@ -3,12 +3,11 @@ const getRestCursor = require('../RestCursor').getRestCursor;
 const fillSchema = require('fill-schema');
 const Controller = require('./Controller');
 const getResourceName = require('./utils/getResourceName');
-const updateDatabase = require('./utils/updateDatabase');
 
 const CanCan = require('cancancan');
 
 const controllerServiceProvider = (dependencies) => Controller.bind(null, dependencies);
-const controller = controllerServiceProvider({getRestFilters, getRestCursor, getResourceName, updateDatabase, fillSchema, CanCan});
+const controller = controllerServiceProvider({ getRestFilters, getRestCursor, getResourceName, fillSchema, CanCan });
 
 module.exports = {
   controllerServiceProvider,
