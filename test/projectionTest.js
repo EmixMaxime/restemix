f63f106ac7398032c6f7c4ca21201cc9c71a4276
EmixMaxime <emixmaxime@outlook.fr>
2017-06-28 11:10:51 +0200

    Cleaning things

--- test/projectionTest.js
+++ test/projectionTest.js
@@ -3,7 +3,6 @@ const expect = chai.expect
 const projection = require('../http/filters/projection')
 const BadRequestError = require('../http/BadRequestError')
 
-
 describe('#projection', () => {
 
   it('It should returns an empty Object without includes | excludes query', () => {
@@ -13,23 +12,16 @@ describe('#projection', () => {
 
   it('It should throws an error with excludes and includes query', () => {
     const fakeRequest = {
-      query: {
-        includes: 'something',
-        excludes: 'something',
-      }
+      query: { includes: 'something',  excludes: 'something' }
     }
 
-    expect(() =>
-      projection(fakeRequest))
-    .to.throw(BadRequestError);
-
+    expect(() => projection(fakeRequest))
+    .to.throw(BadRequestError)
   })
 
   it('It should returns includes Object', () => {
     const fakeRequest = {
-      query: {
-        includes: 'title,author',
-      }
+      query: { includes: 'title,author' }
     }
 
     const result = projection(fakeRequest);
@@ -38,9 +30,7 @@ describe('#projection', () => {
 
   it('It should returns excludes Object', () => {
     const fakeRequest = {
-      query: {
-        excludes: 'title,author',
-      }
+      query: { excludes: 'title,author' }
     }
 
     const result = projection(fakeRequest);
