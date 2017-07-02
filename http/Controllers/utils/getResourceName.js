9e27d7be425d968079e1ab3584fef7564e696f91
EmixMaxime <emixmaxime@outlook.fr>
2017-07-02 16:28:28 +0200

    Cleaning: getResourceName

--- http/Controllers/utils/getResourceName.js
+++ http/Controllers/utils/getResourceName.js
@@ -1,7 +1,10 @@
 /**
-  req url : /categories/php
-  return -> categories
- Rappel : req.baseUrl -> /categories je dois donc enlever le premier /
-*/
-const getResourceName = (req) => req.baseUrl.replace(/^\//, '');
-module.exports = getResourceName;
+ *
+ * @param req
+ * @example
+ * // returns "categories"
+ * req.baseUrl = "/categories/php"
+ * Rappel : req.baseUrl -> /categories je dois donc enlever le premier /
+ */
+const getResourceName = req => req.baseUrl.replace(/^\//, '')
+module.exports = getResourceName
